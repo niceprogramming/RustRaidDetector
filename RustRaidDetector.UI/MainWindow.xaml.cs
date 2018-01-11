4f27ace5869aa93a7f72b09f4c9b45660446dc8b
Adam Kallenbach <adamkallenbach@gmail.com>
2018-01-11 12:11:59 -0600

    Viewmodel detects and populates devices

--- RustRaidDetector.UI/MainWindow.xaml.cs
+++ RustRaidDetector.UI/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +15,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ReactiveUI;
 
 namespace RustRaidDetector.UI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow :IViewFor<MainWindowViewModel>
     {
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register(nameof(ViewModel), typeof(MainWindowViewModel), typeof(MainWindow),
+                new PropertyMetadata(null));
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel = new MainWindowViewModel();
+
+            this.WhenActivated(d =>
+                {
+                    d(this.OneWayBind(ViewModel, vm => vm.Devices, window => window.DevicesComboBox.ItemsSource));
+                    d(this.Bind(ViewModel, vm => vm.SelectedDevice, window => window.DevicesComboBox.SelectedItem));
+                });
+
+            this.Events().Loaded.Select(x => Unit.Default).InvokeCommand(ViewModel.UpdateDevices);
+        }
+
+        
+
+        public MainWindowViewModel ViewModel
+        {
+            get => (MainWindowViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = value as MainWindowViewModel;
         }
     }
 }
---
+++ RustRaidDetector.UI/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Reactive.Linq;
+using CSCore.CoreAudioAPI;
+using CSCore.Streams.Effects;
+using ReactiveUI;
+
+namespace RustRaidDetector.UI
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        private ObservableCollection<MMDevice> _devices;
+        private MMDevice _selectedDevice;
+        private readonly MMDeviceEnumerator _deviceEnumerator;
+        private MMNotificationClient _notificationClient;
+
+        public MainWindowViewModel()
+        {
+            _deviceEnumerator = new MMDeviceEnumerator();
+            _notificationClient = new MMNotificationClient(_deviceEnumerator);
+       
+
+            UpdateDevices = ReactiveCommand.Create(() =>
+            {
+                Devices.Clear();
+                foreach (var device in _deviceEnumerator.EnumAudioEndpoints(DataFlow.All, DeviceState.Active))
+                {
+                    Devices.Add(device);
+                }
+            });
+
+            
+            Observable.Merge(
+                    Observable.FromEventPattern<DeviceNotificationEventArgs>(
+                        x => _notificationClient.DeviceAdded += x,
+                        x => _notificationClient.DeviceAdded -= x),
+                    Observable.FromEventPattern<DeviceNotificationEventArgs>(
+                        y => _notificationClient.DeviceRemoved += y,
+                        y => _notificationClient.DeviceRemoved -= y))
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Select(x => Unit.Default)
+                .InvokeCommand(UpdateDevices);
+
+            Observable.FromEventPattern<DevicePropertyChangedEventArgs>(
+                    z => _notificationClient.DevicePropertyChanged += z,
+                    z => _notificationClient.DevicePropertyChanged -= z)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Select(x => Unit.Default)
+                .InvokeCommand(UpdateDevices);
+
+        }
+
+       
+   
+
+        public ObservableCollection<MMDevice> Devices => _devices ?? (_devices = new ObservableCollection<MMDevice>());
+
+        public MMDevice SelectedDevice
+        {
+            get { return _selectedDevice; }
+            set { this.RaiseAndSetIfChanged(ref _selectedDevice,value); }
+        }
+
+        public ReactiveCommand UpdateDevices { get;}
+    }
+
+  
+}
